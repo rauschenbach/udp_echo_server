9208d5571dda1fbbb5f68f47213e9fb5ae5d0ac7
rauschenbach <boxeur@mail.ru>
2018-11-20 12:11:30 +0300

    Add files via upload
    Частоту снизил до 144 MHz, чтобы частота тактирования SPI SCK была менее 20 MHz

--- library/CMSIS/Device/ST/STM32F4xx/Source/Templates/system_stm32f4xx.c
+++ library/CMSIS/Device/ST/STM32F4xx/Source/Templates/system_stm32f4xx.c
@@ -145,7 +145,7 @@
 
 /* PLL_VCO = (HSE_VALUE or HSI_VALUE / PLL_M) * PLL_N */
 #define PLL_M      8
-#define PLL_N      336
+#define PLL_N      288
 
 /* SYSCLK = PLL_VCO / PLL_P */
 #define PLL_P      2
